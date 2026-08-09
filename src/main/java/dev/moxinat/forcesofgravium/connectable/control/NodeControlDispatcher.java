5dbeacd7e08a6f6c66c61076c8049d2a7506bec1
Moxinat <tv.semix@gmail.com>
2026-08-09 20:04:51 +0200

    Add node control dispatcher placeholder

---
+++ src/main/java/dev/moxinat/forcesofgravium/connectable/control/NodeControlDispatcher.java
@@ -0,0 +1,15 @@
+package dev.moxinat.forcesofgravium.connectable.control;
+
+import com.hypixel.hytale.server.core.universe.world.World;
+import org.joml.Vector3i;
+
+import javax.annotation.Nonnull;
+
+public final class NodeControlDispatcher {
+
+    private NodeControlDispatcher() {
+    }
+
+    public static void dispatch(@Nonnull World world, @Nonnull Vector3i position) {
+    }
+}
