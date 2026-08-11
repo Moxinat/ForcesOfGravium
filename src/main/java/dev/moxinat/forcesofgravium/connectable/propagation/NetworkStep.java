71562529a7ea68a1f9027f032bc8ce5db8b8d164
Moxinat <tv.semix@gmail.com>
2026-08-11 13:19:16 +0200

    remove obsolete network step

--- src/main/java/dev/moxinat/forcesofgravium/connectable/propagation/NetworkStep.java
+++
@@ -1,9 +0,0 @@
-package dev.moxinat.forcesofgravium.connectable.propagation;
-
-import dev.moxinat.forcesofgravium.connectable.SignalState;
-import org.joml.Vector3i;
-
-import javax.annotation.Nonnull;
-
-public record NetworkStep(@Nonnull Vector3i position, @Nonnull SignalState signalState) {
-}
