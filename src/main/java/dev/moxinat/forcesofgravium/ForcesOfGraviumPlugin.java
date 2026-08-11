27d66826dff74208abf08a0bb4bec3e5a3ec2c45
Moxinat <tv.semix@gmail.com>
2026-08-11 20:57:04 +0200

    remove placement rotation system registration

--- src/main/java/dev/moxinat/forcesofgravium/ForcesOfGraviumPlugin.java
+++ src/main/java/dev/moxinat/forcesofgravium/ForcesOfGraviumPlugin.java
@@ -7,7 +7,6 @@ import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import dev.moxinat.forcesofgravium.commands.ForcesOfGraviumCommand;
 import dev.moxinat.forcesofgravium.event.ForcesOfGraviumEvents;
 import dev.moxinat.forcesofgravium.system.ButtonInteractionSystem;
-import dev.moxinat.forcesofgravium.system.BlockPlacementRotationSystem;
 import dev.moxinat.forcesofgravium.system.ConnectableBlockLifecycleSystem;
 import dev.moxinat.forcesofgravium.system.ConnectablePropagationSystem;
 import dev.moxinat.forcesofgravium.system.CurveCasedGravityPowderRotationSystem;
@@ -27,7 +26,6 @@ public class ForcesOfGraviumPlugin extends JavaPlugin {
         );
         this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, ForcesOfGraviumEvents::onPlayerReady);
         this.getEventRegistry().registerGlobal(ShutdownEvent.class, ForcesOfGraviumEvents::onShutdown);
-        this.getEntityStoreRegistry().registerSystem(new BlockPlacementRotationSystem());
         this.getEntityStoreRegistry().registerSystem(new ButtonInteractionSystem());
         this.getEntityStoreRegistry().registerSystem(new CurveCasedGravityPowderRotationSystem.UseSystem());
         this.getEntityStoreRegistry().registerSystem(new ConnectableBlockLifecycleSystem.PlaceSystem());
