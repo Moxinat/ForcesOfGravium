5eb21bc6070c72d9dbaf50ab269e1e68d343563e
Moxinat <tv.semix@gmail.com>
2026-08-12 16:23:49 +0200

    add node state dispatcher

---
+++ src/main/java/dev/moxinat/forcesofgravium/connectable/dispatcher/NodeStateDispatcher.java
@@ -0,0 +1,15 @@
+package dev.moxinat.forcesofgravium.connectable.dispatcher;
+
+import com.hypixel.hytale.server.core.universe.world.World;
+import org.joml.Vector3i;
+
+import javax.annotation.Nonnull;
+
+public final class NodeStateDispatcher {
+
+    private NodeStateDispatcher() {
+    }
+
+    public static void dispatch(@Nonnull World world, @Nonnull Vector3i position) {
+    }
+}
