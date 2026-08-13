2887ee5096dbb56aec3de71b951bdeaaa1367865
Moxinat <tv.semix@gmail.com>
2026-08-13 20:46:45 +0200

    remove inverter capability flag usage

--- src/main/java/dev/moxinat/forcesofgravium/block/inverter/InverterStateCalculator.java
+++ src/main/java/dev/moxinat/forcesofgravium/block/inverter/InverterStateCalculator.java
@@ -1,9 +1,9 @@
 package dev.moxinat.forcesofgravium.block.inverter;
 
+import com.hypixel.hytale.server.core.universe.world.World;
 import dev.moxinat.forcesofgravium.connectable.SignalState;
 import dev.moxinat.forcesofgravium.data.Nodes;
 import org.joml.Vector3i;
-import com.hypixel.hytale.server.core.universe.world.World;
 
 public final class InverterStateCalculator {
 
@@ -15,29 +15,22 @@ public final class InverterStateCalculator {
             Vector3i inverterPosition,
             Vector3i controlSourcePosition
     ) {
-        Nodes.Node controlSource =
-                Nodes.get(world, controlSourcePosition);
+        Nodes.Node controlSource = Nodes.get(world, controlSourcePosition);
 
         if (controlSource == null
                 || controlSource.effectiveState() == SignalState.OFF) {
             return;
         }
 
-        Nodes.Node inverter =
-                Nodes.get(world, inverterPosition);
+        Nodes.Node inverter = Nodes.get(world, inverterPosition);
 
-        if (inverter == null
-                || !inverter.invertCapable()) {
+        if (inverter == null) {
             return;
         }
 
-        inverter = inverter.withInvertEnabled(
-                !inverter.invertEnabled()
-        );
-
         Nodes.put(
                 world,
-                inverter
+                inverter.withInvertEnabled(!inverter.invertEnabled())
         );
     }
 }
