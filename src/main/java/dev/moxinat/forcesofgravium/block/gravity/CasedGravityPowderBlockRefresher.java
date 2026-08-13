4b702d2172e665aaff20c459105a7911be2c7399
Moxinat <tv.semix@gmail.com>
2026-08-13 10:06:40 +0200

    updated all refreshers

--- src/main/java/dev/moxinat/forcesofgravium/block/gravity/CasedGravityPowderBlockRefresher.java
+++ src/main/java/dev/moxinat/forcesofgravium/block/gravity/CasedGravityPowderBlockRefresher.java
@@ -2,11 +2,10 @@ package dev.moxinat.forcesofgravium.block.gravity;
 
 import com.hypixel.hytale.math.util.ChunkUtil;
 import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
-import com.hypixel.hytale.server.core.asset.type.blocktype.config.RotationTuple;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.accessor.BlockAccessor;
-import dev.moxinat.forcesofgravium.connectable.core.ConnectableRuntimeAccessor;
-import dev.moxinat.forcesofgravium.connectable.registry.ConnectableRegistry;
+import dev.moxinat.forcesofgravium.connectable.registry.NodeTypes;
+import dev.moxinat.forcesofgravium.data.Nodes;
 import org.joml.Vector3i;
 
 import javax.annotation.Nullable;
@@ -16,52 +15,97 @@ public final class CasedGravityPowderBlockRefresher {
     private CasedGravityPowderBlockRefresher() {
     }
 
-    public static void refreshAt(World world, Vector3i position) {
-        refreshAt(world, position.x(), position.y(), position.z());
-    }
+    public static void refreshAt(
+            World world,
+            Vector3i position
+    ) {
+        Nodes.Node node = Nodes.get(
+                world,
+                position
+        );
 
-    public static void refreshAt(World world, int x, int y, int z) {
-        BlockType currentType = world.getBlockType(x, y, z);
-        if (currentType == null || !ConnectableRegistry.isCasedGravityPowderId(currentType.getId())) {
+        if (node == null) {
             return;
         }
 
-        String baseBlockId = baseBlockIdFor(currentType.getId());
-        if (baseBlockId == null) {
+        String baseBlockId;
+
+        if (NodeTypes.STRAIGHT_CASED_GRAVITY_POWDER
+                .blockId()
+                .equals(node.blockId())) {
+
+            baseBlockId =
+                    NodeTypes.STRAIGHT_CASED_GRAVITY_POWDER.blockId();
+
+        } else if (NodeTypes.CURVE_CASED_GRAVITY_POWDER
+                .blockId()
+                .equals(node.blockId())) {
+
+            baseBlockId =
+                    NodeTypes.CURVE_CASED_GRAVITY_POWDER.blockId();
+
+        } else {
             return;
         }
 
-        BlockType baseType = BlockType.fromString(baseBlockId);
+        int x = position.x();
+        int y = position.y();
+        int z = position.z();
+
+        BlockType currentType =
+                world.getBlockType(x, y, z);
+
+        if (currentType == null) {
+            return;
+        }
+
+        BlockType baseType =
+                BlockType.fromString(baseBlockId);
+
         if (baseType == null) {
             return;
         }
 
-        Vector3i position = new Vector3i(x, y, z);
-        String stateName = ConnectableRuntimeAccessor.stateNameForSignalState(
-                ConnectableRuntimeAccessor.effectiveState(world, position)
-        );
-        String blockKey = baseType.getBlockKeyForState(stateName);
+        String stateName = switch (node.effectiveState()) {
+            case PUSH -> "Push";
+            case PULL -> "Pull";
+            case OFF -> "Off";
+        };
+
+        String blockKey =
+                baseType.getBlockKeyForState(
+                        stateName
+                );
+
         if (blockKey == null) {
             blockKey = baseBlockId;
         }
 
-        BlockAccessor chunk = world.getChunk(ChunkUtil.indexChunkFromBlock(x, z));
+        if (blockKey.equals(currentType.getId())) {
+            return;
+        }
+
+        BlockAccessor chunk =
+                world.getChunk(
+                        ChunkUtil.indexChunkFromBlock(
+                                x,
+                                z
+                        )
+                );
+
         if (chunk == null) {
             return;
         }
 
-        RotationTuple rotation = ConnectableRuntimeAccessor.rotation(world, position);
-        chunk.placeBlock(x, y, z, blockKey, rotation, 0, false);
-    }
-
-    private static @Nullable String baseBlockIdFor(@Nullable String blockId) {
-        if (ConnectableRegistry.isStraightCasedGravityPowderId(blockId)) {
-            return ConnectableRegistry.STRAIGHT_CASED_GRAVITY_POWDER_BLOCK_ID;
-        }
-        if (ConnectableRegistry.isCurveCasedGravityPowderId(blockId)) {
-            return ConnectableRegistry.CURVE_CASED_GRAVITY_POWDER_BLOCK_ID;
-        }
-        return null;
+        chunk.placeBlock(
+                x,
+                y,
+                z,
+                blockKey,
+                node.rotation(),
+                0,
+                false
+        );
     }
 
 }
--- src/main/java/dev/moxinat/forcesofgravium/connectable/dispatcher/ConnectableVisualDispatcher.java
+++ src/main/java/dev/moxinat/forcesofgravium/connectable/dispatcher/ConnectableVisualDispatcher.java
@@ -1,7 +1,13 @@
 package dev.moxinat.forcesofgravium.connectable.dispatcher;
 
 import com.hypixel.hytale.server.core.universe.world.World;
+import dev.moxinat.forcesofgravium.block.gravity.CasedGravityPowderBlockRefresher;
+import dev.moxinat.forcesofgravium.block.gravity.GravityPowderBlockRefresher;
+import dev.moxinat.forcesofgravium.block.inverter.InverterBlockRefresher;
+import dev.moxinat.forcesofgravium.block.siphon.GraviumSiphonBlockRefresher;
+import dev.moxinat.forcesofgravium.connectable.registry.ConnectableRegistry;
 import dev.moxinat.forcesofgravium.connectable.spatial.ConnectableNeighborResolver;
+import dev.moxinat.forcesofgravium.data.Nodes;
 import org.joml.Vector3i;
 
 import javax.annotation.Nonnull;
@@ -15,6 +21,26 @@ public final class ConnectableVisualDispatcher {
             @Nonnull World world,
             @Nonnull Vector3i position
     ) {
+        Nodes.Node node = Nodes.get(world, position);
+
+        if (node == null) {
+            return;
+        }
+
+        switch (node.blockId()) {
+            case ConnectableRegistry.GRAVIUM_SIPHON_BLOCK_ID ->
+                    GraviumSiphonBlockRefresher.refreshAt(world, position);
+            case ConnectableRegistry.INVERTER_BLOCK_ID ->
+                    InverterBlockRefresher.refreshAt(world, position);
+            case ConnectableRegistry.STRAIGHT_CASED_GRAVITY_POWDER_BLOCK_ID,
+                 ConnectableRegistry.CURVE_CASED_GRAVITY_POWDER_BLOCK_ID ->
+                    CasedGravityPowderBlockRefresher.refreshAt(world, position);
+            case ConnectableRegistry.GRAVITY_POWDER_BLOCK_ID ->
+                    GravityPowderBlockRefresher.refreshAt(world, position);
+
+            default -> {
+            }
+        }
     }
 
     public static void refreshTopologyAround(
