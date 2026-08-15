9d2df871de0e56ded4a6429bacfdc9388bbdc119
Moxinat <tv.semix@gmail.com>
2026-08-15 08:10:18 +0200

    add network energy check

--- src/main/java/dev/moxinat/forcesofgravium/connectable/energy/EnergyManager.java
+++ src/main/java/dev/moxinat/forcesofgravium/connectable/energy/EnergyManager.java
@@ -1,7 +1,42 @@
 package dev.moxinat.forcesofgravium.connectable.energy;
 
+import com.hypixel.hytale.server.core.universe.world.World;
+import dev.moxinat.forcesofgravium.data.Nodes;
+import org.joml.Vector3i;
+
+import javax.annotation.Nonnull;
+
 public final class EnergyManager {
 
     private EnergyManager() {
+    }
+
+    public static void checkNetwork(
+            @Nonnull World world,
+            @Nonnull Vector3i position
+    ) {
+        Nodes.Node node = Nodes.get(world, position);
+
+        if (node == null || node.networkId() == Nodes.Node.NO_NETWORK) {
+            return;
+        }
+
+        long networkId = node.networkId();
+        int energy = Nodes.snapshotForWorld(world)
+                .values()
+                .stream()
+                .filter(networkNode -> networkNode.networkId() == networkId)
+                .mapToInt(Nodes.Node::energyDelta)
+                .sum();
+
+        if (energy < 0) {
+            failNetwork(world, networkId);
+        }
+    }
+
+    private static void failNetwork(
+            @Nonnull World world,
+            long networkId
+    ) {
     }
 }
